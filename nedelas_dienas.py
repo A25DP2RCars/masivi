67079bb761fafcf9f8cba40b76601c8a1def0041
Ričards Cars <A250974RC@rvt.lv>
2026-06-08 10:01:11 +0300

    Add files via upload

---
+++ nedelas_dienas.py
@@ -0,0 +1,9 @@
+nedelas_dienas = ['pirmdiena','otrdiena','trešdiena','ceturtdiena','piektdiena','sestdiena','svētdiena']
+print(nedelas_dienas[0])
+print(nedelas_dienas[1])
+print(nedelas_dienas[2])
+print(nedelas_dienas[3])
+print(nedelas_dienas[4])
+print(nedelas_dienas[5])
+print(nedelas_dienas[6])
+print(nedelas_dienas[7])
