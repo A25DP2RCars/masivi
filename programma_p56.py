1ed50d475f92ee99ea8bcf95d3e00120f88cd516
Ričards Cars <A250974RC@rvt.lv>
2026-06-08 10:02:38 +0300

    Add files via upload

---
+++ programma_p56.py
@@ -0,0 +1,9 @@
+#programma p56
+sk = [0, 0, 0]
+
+sk[0] = int(input('ievadiet 1.skaitli:'))
+sk[1] = int(input('ievadiet 2.skaitli:'))
+sk[2] = int(input('ievadiet 3.skaitli:'))
+
+summa = sk[0] + sk[1] + sk[2]
+print(summa)
