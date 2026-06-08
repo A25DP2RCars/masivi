7562b75ee1405fbbb881f16de23fea75a71f0e11
Ričards Cars <A250974RC@rvt.lv>
2026-06-08 10:02:17 +0300

    Add files via upload

---
+++ 3_locekli.py
@@ -0,0 +1,8 @@
+sk = [0, 0, 0] # Šajā brīdi izveidojas 3 atminas kastītes ar 0
+
+sk[0] = 45
+sk[1] = 56
+sk[2] = 55 #šajā brīdi katra kastīte ir vērtība, ar kuru iespējams darboties
+
+summa = sk[0] + sk[1] + sk[2]
+print(summa)
